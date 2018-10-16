213fb6c6427f00ce3cad73237ddb3cd24f78a451
Tangjiafeng <1282141182@qq.com>
2018-10-16 21:43:02 +0800

    常用 transformation 操作

--- spark-study-scala/src/cn/spark/study/core/WordCount.scala
+++ spark-study-scala/src/cn/spark/study/core/WordCount.scala
@@ -13,8 +13,8 @@ object WordCount {
         .setAppName("WordCount");
     val sc = new SparkContext(conf)
   
-    val lines = sc.textFile("hdfs://spark1:9000/spark.txt", 1); 
-    val words = lines.flatMap { line => line.split(" ") }   
+    val lines = sc.textFile("hdfs://spark1:9000/spark.txt", 1) 
+    val words = lines.flatMap { line => line.split(" ") }
     val pairs = words.map { word => (word, 1) }   
     val wordCounts = pairs.reduceByKey { _ + _ }
     
